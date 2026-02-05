7a615ac65c30fae9772864208ba6fd7e6b6e0c9b
AlbinJoseph07 <albinjo2025@gmail.com>
2026-02-05 23:42:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    let formData = new FormData(this);
-
-    fetch("save_contact.php", {
-        method: "POST",
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("response").innerText = data;
-        document.getElementById("contactForm").reset();
-    });
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("response").innerText =
+    "Thank you! Contact form will be active soon.";
 });
+
