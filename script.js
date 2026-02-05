68848f45feaa271508b5dc4bb5a9af9702fcd551
AlbinJoseph07 <albinjo2025@gmail.com>
2026-02-06 00:08:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  document.getElementById("response").innerText =
-    "Thank you! Contact form will be active soon.";
-});
+
 
